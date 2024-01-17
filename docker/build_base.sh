f5a353c8810f418488a9435965036551797399a8
Eugene Chai <eugene.chai@nokia-bell-labs.com>
2024-01-17 12:21:34 -0500

    Updated docker base info

--- docker/build_base.sh
+++ docker/build_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=1.0
-BASE_IMAGE=gaussian_splatting/3dgs-base:${VERSION}
-CONTAINER_NAME=3dgs-container
-IMAGE_NAME=gaussian_splatting/3dgs-image:${VERSION}
+BASE_IMAGE=gaussian_splatting/ref_3dgs-base:${VERSION}
+CONTAINER_NAME=ref_3dgs-container
+IMAGE_NAME=gaussian_splatting/ref_3dgs-image:${VERSION}
 
